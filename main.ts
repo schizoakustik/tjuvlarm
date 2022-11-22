b7229ef5b0c08f50133901f0d5caa785a96ccea8
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2022-11-22 11:24:54 +0100

    adds code, pin outputs

--- main.ts
+++ main.ts
@@ -1,5 +1,45 @@
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (insertCode() == 0) {
+        larmat = !(larmat)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else {
+        basic.showNumber(2)
+    }
+})
+function insertCode () {
+    basic.clearScreen()
+    inserted = ""
+    while (inserted.length < 4) {
+        if (input.buttonIsPressed(Button.A)) {
+            inserted = "" + inserted + "A"
+            basic.pause(200)
+            basic.clearScreen()
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            inserted = "" + inserted + "B"
+            basic.pause(200)
+            basic.clearScreen()
+        }
+        led.plot(inserted.length, 4)
+    }
+    basic.clearScreen()
+    return kod.compare(inserted)
+}
+let inserted = ""
+let kod = ""
+let larmat = false
+larmat = false
+kod = "ABBA"
+pins.setAudioPin(AnalogPin.P0)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Z) > 150) {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    if (larmat) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        if (input.acceleration(Dimension.Z) > 150) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+        } else {
+            basic.clearScreen()
+        }
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
